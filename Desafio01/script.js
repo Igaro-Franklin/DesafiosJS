2c2ba40aec513d2be7832eb5a2ae2e62a48394ab
Igaro-Franklin <igarofranklinr@gmail.com>
2023-05-15 22:09:02 -0300

    Finalizando a 1ª missão

---
+++ Desafio01/script.js
@@ -0,0 +1,12 @@
+var nome = prompt("Informe seu nome", "Digite aqui");
+var identidade = document.getElementById("identificacao");
+
+identidade.innerHTML = nome;
+
+
+
+var idade = prompt("Qual a sua idade:", "Digite aqui");
+var anos = document.getElementById("anos");
+
+anos.innerHTML = idade;
+
